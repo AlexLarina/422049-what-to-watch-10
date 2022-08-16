9b95ebafc22c8d07786adc6882456c7a9a7c0f41
Alexandra Larina <aa.larina.ff@gmail.com>
2022-08-16 22:36:00 +0300

    Fixes: styling in private-route component

--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -1,6 +1,9 @@
-import { AppRoute, AuthStatus } from '../../const';
+import {
+  AppRoute,
+  AuthStatus
+} from '../../const';
 
-import {Navigate} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../../hooks';
 
 type PrivateRouteProps = {
