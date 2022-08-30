294a5f17ce33f9db419bda0d63736758ef36fc5f
Alexandra Larina <aa.larina.ff@gmail.com>
2022-08-30 16:52:00 +0300

    Moving HistoryRouter from App component to root

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -5,7 +5,6 @@ import {
 
 import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
 import { AppRoute } from '../../const';
-import HistoryRouter from '../history-route/history-route';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
 import MovieScreen from '../../pages/movie-screen/movie-screen';
@@ -13,7 +12,6 @@ import MyListScreen from '../../pages/my-list-screen/my-list-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import PlayerScreen from '../../pages/player-screen/player-screen';
 import PrivateRoute from '../private-route/private-route';
-import browserHistory from '../../services/browser-history';
 import { useAppSelector } from '../../hooks';
 
 function App(): JSX.Element {
@@ -26,55 +24,53 @@ function App(): JSX.Element {
   }
 
   return (
-    <HistoryRouter history={browserHistory}>
-      <Routes>
-        <Route
-          path={AppRoute.Root}
-          element={
-            <MainScreen />
-          }
-        />
-        <Route
-          path={AppRoute.Login}
-          element={<LoginScreen />}
-        />
-        <Route
-          path={AppRoute.UserFilmList}
-          element={
-            <PrivateRoute>
-              <MyListScreen />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path={AppRoute.Film}
-          element={
-            <MovieScreen />
-          }
-        />
-        <Route
-          path={AppRoute.NewReview}
-          element={
-            <PrivateRoute>
-              <AddReviewScreen />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path={AppRoute.Player}
-          element={
-            <PlayerScreen />
-          }
-        />
-        <Route
-          path={AppRoute.NotFound}
-          element={
-            <NotFoundScreen />
-          }
-        />
-        <Route path='*' element={<NotFoundScreen/>}/>
-      </Routes>
-    </HistoryRouter>
+    <Routes>
+      <Route
+        path={AppRoute.Root}
+        element={
+          <MainScreen />
+        }
+      />
+      <Route
+        path={AppRoute.Login}
+        element={<LoginScreen />}
+      />
+      <Route
+        path={AppRoute.UserFilmList}
+        element={
+          <PrivateRoute>
+            <MyListScreen />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path={AppRoute.Film}
+        element={
+          <MovieScreen />
+        }
+      />
+      <Route
+        path={AppRoute.NewReview}
+        element={
+          <PrivateRoute>
+            <AddReviewScreen />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path={AppRoute.Player}
+        element={
+          <PlayerScreen />
+        }
+      />
+      <Route
+        path={AppRoute.NotFound}
+        element={
+          <NotFoundScreen />
+        }
+      />
+      <Route path='*' element={<NotFoundScreen/>}/>
+    </Routes>
   );
 }
 
