410392b12a84339b726ffb13d67695058338ad06
Alexandra Larina <aa.larina.ff@gmail.com>
2022-08-30 22:35:06 +0300

    Adding genre-list component test

---
+++ project/src/components/genre-list/genre-list.test.tsx
@@ -0,0 +1,31 @@
+import {
+  render,
+  screen
+} from '@testing-library/react';
+
+import AddReview from './add-review';
+import { FILM_MOCK_AMOUNT } from '../../const';
+import GenreList from './genre-list';
+import HistoryRouter from '../history-route/history-route';
+import { Provider } from 'react-redux';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { createMemoryHistory } from 'history';
+import { makeFakeFilm } from '../../test/mocks';
+
+const mockStore = configureMockStore();
+
+describe('Component: GenreList', () => {
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+    const films = new Array(FILM_MOCK_AMOUNT).fill(makeFakeFilm());
+    render(
+      <Provider store={mockStore({})}>
+        <HistoryRouter history={history}>
+          <GenreList filmData={films}/>
+        </HistoryRouter>
+      </Provider>,
+    );
+
+    expect(screen.getByText('All genres')).toBeInTheDocument();
+  });
+});
