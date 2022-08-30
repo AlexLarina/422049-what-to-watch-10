6e3d0f90f5fcc3c61f52afadf73268fbc34cf7bc
Alexandra Larina <aa.larina.ff@gmail.com>
2022-08-30 16:05:19 +0300

    Adding not found screen component test

---
+++ project/src/pages/not-found-screen/not-found-screen.test.tsx
@@ -0,0 +1,33 @@
+import {
+  render,
+  screen
+} from '@testing-library/react';
+
+import HistoryRouter from '../../components/history-route/history-route';
+import NotFoundScreen from './not-found-screen';
+import { Provider } from 'react-redux';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { createMemoryHistory } from 'history';
+
+const mockStore = configureMockStore();
+
+describe('Component: NotFoundScreen', () => {
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+    history.push('/not_found');
+
+    render(
+      <Provider store={mockStore({})}>
+        <HistoryRouter history={history}>
+          <NotFoundScreen />
+        </HistoryRouter>
+      </Provider>,
+    );
+
+    const headerElement = screen.getByText('404');
+    const linkElement = screen.getByText('main page');
+
+    expect(headerElement).toBeInTheDocument();
+    expect(linkElement).toBeInTheDocument();
+  });
+});
