5f3013e2c98e0799573a0b027cda4a48530b3461
Alexandra Larina <aa.larina.ff@gmail.com>
2022-08-30 22:39:37 +0300

    Adding header component test

--- project/src/components/header/header.test.tsx
+++ project/src/components/header/header.test.tsx
@@ -3,28 +3,26 @@ import {
   screen
 } from '@testing-library/react';
 
-import { FILM_MOCK_AMOUNT } from '../../const';
-import GenreList from './genre-list';
+import Header from './header';
 import HistoryRouter from '../history-route/history-route';
 import { Provider } from 'react-redux';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createMemoryHistory } from 'history';
-import { makeFakeFilm } from '../../test/mocks';
 
 const mockStore = configureMockStore();
 
-describe('Component: GenreList', () => {
+describe('Component: Header', () => {
   it('should render correctly', () => {
     const history = createMemoryHistory();
-    const films = new Array(FILM_MOCK_AMOUNT).fill(makeFakeFilm());
+
     render(
       <Provider store={mockStore({})}>
         <HistoryRouter history={history}>
-          <GenreList filmData={films}/>
+          <Header headerClass={''}/>
         </HistoryRouter>
       </Provider>,
     );
 
-    expect(screen.getByText('All genres')).toBeInTheDocument();
+    expect(screen.getByText('T')).toBeInTheDocument();
   });
 });
