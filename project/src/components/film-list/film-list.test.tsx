f25562a725c6c92170787c767251a793c84a017a
Alexandra Larina <aa.larina.ff@gmail.com>
2022-08-30 22:22:03 +0300

    Adding film-list component test

---
+++ project/src/components/film-list/film-list.test.tsx
@@ -0,0 +1,36 @@
+import {
+  render,
+  screen
+} from '@testing-library/react';
+
+import { AuthStatus } from '../../const';
+import FilmList from './film-list';
+import HistoryRouter from '../history-route/history-route';
+import { Provider } from 'react-redux';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { createMemoryHistory } from 'history';
+import { makeFakeFilm } from '../../test/mocks';
+
+const mockStore = configureMockStore();
+
+describe('Component: FilmList', () => {
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+    const films = new Array(2).fill(makeFakeFilm());
+    render(
+      <Provider store={
+        mockStore({
+          favouriteFilmList: new Array(3),
+          authStatus: AuthStatus.Auth
+        })
+      }
+      >
+        <HistoryRouter history={history}>
+          <FilmList filmData={films}/>
+        </HistoryRouter>
+      </Provider>,
+    );
+
+    expect(screen.getByTestId('catalog__films-list')).toBeInTheDocument();
+  });
+});
