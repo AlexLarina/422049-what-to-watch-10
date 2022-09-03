bbad26dc170116feabfa17218f88e53800783c34
Alexandra Larina <aa.larina.ff@gmail.com>
2022-09-03 14:34:54 +0300

    Adding selectors

---
+++ project/src/store/film-process/selectors.ts
@@ -0,0 +1,31 @@
+// fullFilmList: [],
+// filmList: [],
+
+import Film from '../../types/film';
+import { NameSpace } from '../../const';
+import { State } from '../../types/state';
+
+export const getGenre = (state: State): string => (
+  state[NameSpace.Film].genre
+);
+
+export const getLoadingStatus = (state: State):
+  {promo: boolean, films: boolean, favourite: boolean} => (
+  state[NameSpace.Film].isLoadingCompleted
+);
+
+export const getPromo = (state: State): Film | null => (
+  state[NameSpace.Film].promo
+);
+
+export const getFavourites = (state: State): Film[] => (
+  state[NameSpace.Film].favouriteFilmList
+);
+
+export const getFilms = (state: State): Film[] => (
+  state[NameSpace.Film].filmList
+);
+
+export const getAllFilms = (state: State): Film[] => (
+  state[NameSpace.Film].fullFilmList
+);
