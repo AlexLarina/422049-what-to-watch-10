3758f7a09eab4ffc5b9e6e115926d030dc9e3473
Alexandra Larina <aa.larina.ff@gmail.com>
2022-09-04 12:00:34 +0300

    Fixes: review screen test

--- project/src/pages/add-review-screen/add-review-screen.test.tsx
+++ project/src/pages/add-review-screen/add-review-screen.test.tsx
@@ -4,21 +4,42 @@ import {
 } from '@testing-library/react';
 
 import AddReviewScreen from './add-review-screen';
+import { AuthStatus } from '../../const';
 import HistoryRouter from '../../components/history-route/history-route';
+import { Provider } from 'react-redux';
+import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createMemoryHistory } from 'history';
+
+const mockStore = configureMockStore();
+
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useLocation: () => ({
+    state: {
+      film: {},
+    }
+  })
+}));
 
 describe('Component: AddReviewScreen', () => {
   it('should render correctly', () => {
-    // const history = createMemoryHistory();
+    const history = createMemoryHistory();
 
-    // render(
-    //   <HistoryRouter history={history}>
-    //     <AddReviewScreen />
-    //   </HistoryRouter>,
-    // );
+    render(
+      <Provider store={mockStore({
+        USER: {
+          authorizationStatus: AuthStatus.NotAuth,
+        }
+      })}
+      >
+        <HistoryRouter history={history}>
+          <AddReviewScreen />
+        </HistoryRouter>
+      </Provider>,
+    );
 
-    // const spanElement = screen.getByText('Add review');
+    const spanElement = screen.getByText('Add review');
 
-    // expect(spanElement).toBeInTheDocument();
+    expect(spanElement).toBeInTheDocument();
   });
 });
