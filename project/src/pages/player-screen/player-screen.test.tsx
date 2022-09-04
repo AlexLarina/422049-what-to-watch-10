8d9c35765aff717dfc12996aea59f0446f747e20
Alexandra Larina <aa.larina.ff@gmail.com>
2022-09-04 12:21:58 +0300

    Fixes: player screen test

--- project/src/pages/player-screen/player-screen.test.tsx
+++ project/src/pages/player-screen/player-screen.test.tsx
@@ -11,16 +11,29 @@ import { createMemoryHistory } from 'history';
 
 const mockStore = configureMockStore();
 
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useLocation: () => ({
+    state: {
+      filmRef: '',
+      poster: '',
+    }
+  })
+}));
+
 describe('Component: PlayerScreen', () => {
   it('should render correctly', () => {
-    // const history = createMemoryHistory();
+    const history = createMemoryHistory();
 
-    // render(
-    //   <Provider store={mockStore({})}>
-    //     <HistoryRouter history={history}>
-    //       <PlayerScreen />
-    //     </HistoryRouter>
-    //   </Provider>,
-    // );
+    render(
+      <Provider store={mockStore({})}>
+        <HistoryRouter history={history}>
+          <PlayerScreen />
+        </HistoryRouter>
+      </Provider>,
+    );
+
+    expect(screen.getByText('Transpotting')).toBeInTheDocument();
+    expect(screen.getByText('Full screen')).toBeInTheDocument();
   });
 });
